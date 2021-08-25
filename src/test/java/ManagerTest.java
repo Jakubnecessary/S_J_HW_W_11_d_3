026c0a1ed6f5e9e5b13005bf240ac1fcdd8bd2fa
jakubnecessary <cabbage1991@gmail.com>
2021-08-25 17:02:22 +0100

    raiseSalary tested and working

---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,19 @@
+import management.Manager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+    Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("Stephen", 5, 100000, "Hell");
+    }
+    @Test
+    public void canRaiseSalary() {
+        manager.raiseSalary(1000);
+        assertEquals(101000, manager.getSalary(), 0.1);
+    }
+}
